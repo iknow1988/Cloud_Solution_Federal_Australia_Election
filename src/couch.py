ea64b92dcd4e97a5d185f930a7676fb6f095e20f
Kazi Abir Adnan <i_know1988@yahoo.com>
2019-04-25 19:06:46 +1000

    Script to update json documents

--- src/couch.py
+++ src/couch.py
@@ -1,8 +1,22 @@
 import couchdb
+import json
 
-couchserver = couchdb.Server("http://115.146.85.64:9584/")
+couchserver = couchdb.Server("http://admin:group2@115.146.84.108:9584/")
+new_db = couchserver['raw_twitter']
 
-for dbname in couchserver:
-    print(dbname)
+def main():
+    db = couchserver['twitter']
+    print(len(db))
+    for index, id in enumerate(db):
+        doc = db[id]
+        for i in doc:
+            if i != '_id' and i != '_rev':
+                try:
+                    new_db[i] = doc[i]
+                    print(index)
+                except:
+                    print(index, " already exists")
+    print("done")
 
-print("Done")
+if __name__ == "__main__":
+	main()
