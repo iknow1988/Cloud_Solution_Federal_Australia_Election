69255185b0314004587fc071b6c2eb67ceff1ad0
Kazi Abir Adnan <i_know1988@yahoo.com>
2019-04-26 12:37:07 +1000

    Started another harvester

--- src/couch.py
+++ src/couch.py
@@ -2,10 +2,10 @@ import couchdb
 import json
 
 couchserver = couchdb.Server("http://admin:group2@115.146.84.108:9584/")
-new_db = couchserver['raw_twitter']
 
-def main():
+def fix_tweets():
     db = couchserver['twitter']
+    new_db = couchserver['raw_twitter']
     print(len(db))
     for index, id in enumerate(db):
         doc = db[id]
@@ -17,6 +17,56 @@ def main():
                 except:
                     print(index, " already exists")
     print("done")
+
+
+def get_users():
+    db = couchserver['raw_twitter']
+    try:
+        new_db = couchserver.create('users_twitter')
+    except:
+        new_db = couchserver['users_twitter']
+    print(len(db))
+    for index, id in enumerate(db):
+        doc = db[id]
+        screen_name = doc['user']['screen_name']
+        user = doc['user']['id_str']
+        if 'processed' not in doc:
+            doc['processed'] = 1
+            if user not in new_db:
+                new_db[user] = {'count': 1, 'tweets': [doc['id_str']], 'screen_name': screen_name}
+            else:
+                data = new_db[user]
+                data['tweets'].append(doc['id_str'])
+                data['count'] = data['count'] + 1
+                data['screen_name'] = screen_name
+                new_db[user] = data
+        else:
+            data = new_db[user]
+            data['screen_name'] = screen_name
+            new_db[user] = data
+            print("Already processed: ", screen_name)
+        print(index)
+        db.save(doc)
+
+
+def copy_to_database():
+    old_db = couchserver['users_timeline_twitter']
+    new_db = couchserver['raw_twitter']
+    rows = len(old_db)
+    for index, id in enumerate(old_db):
+        doc = old_db[id]
+        try:
+            new_db[doc['id_str']] = doc
+        except:
+            print("already there in database")
+            pass
+        print("left:", rows - index)
+
+
+def main():
+    copy_to_database()
+
+    return 0
 
 if __name__ == "__main__":
 	main()
--- src/main.py
+++ src/main.py
@@ -11,6 +11,18 @@ access_token_secret = "BfpYotAPGPA23B9dIcCicHR9r6YzwWFyhQx54SbVyXT6d"
 consumer_key = "FjDcSP67VEq6N0XxIsQpr9BRR"
 consumer_secret = "4qHx0cJvEwXD8u4yxKANVQP1mJ6oFNqHbEpELuo9l3SsSDKl1c"
 
+# (This are all Daniel's credential)
+# access_token = "16618497-snNvcNNuQjYhRCztSQ06dD6jT1lssjz7yLCaUHdiQ"
+# access_token_secret = "vOXVTbz24FwSjUF2fQHDEdnZpER7HYHi3tgJRdO5COn82"
+# consumer_key = "pULdzWMef98hVRYmyyuQXbD05"
+# consumer_secret = "HaCFycsgw4uv010eV8e40nneTQbjAwFN7acIZoEkCU4Chhn0ur"
+
+# Nafis
+# access_token = "2680166335-Qz7Amfi0bc3gYaqnFxHmmH8BJVc10vN9mAOFuLB"
+# access_token_secret = "DwgayvsCmqLaEkugcsssYjf8Zqw12tfgWldq6B2FS7DkV"
+# consumer_key = "DzCD85X4wVcyL6AwPtFRwDxSZ"
+# consumer_secret = "R8oyIchRZ3Y6wpEHQRTAZ14oV3AEZIRa8eLFnMaVo543QXsFkP"
+
 # Couchdb configure
 couch = couchdb.Server('http://admin:group2@115.146.84.108:9584/')
 
@@ -18,6 +30,8 @@ try:
 	db = couch.create('raw_twitter')
 except:
 	db = couch['raw_twitter']
+users_db = couch['users_twitter']
+
 
 # This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
@@ -25,9 +39,22 @@ class StdOutListener(StreamListener):
 	def on_data(self, data):
 		try:
 			data = json.loads(data)
+			data['processed'] = 1
 			db[data['id_str']] = data
+			user = data['user']['id_str']
+			print(data['id_str'], " saved to tweeter database")
+			if user not in users_db:
+				users_db[user] = {'count': 1, 'tweets': [data['id_str']], 'screen_name': data['user']['screen_name']}
+				print("\tnew ",user, "added to user database")
+			else:
+				data_user = users_db[user]
+				data_user['tweets'].append(data['id_str'])
+				data_user['count'] = data_user['count'] + 1
+				data_user['screen_name'] = data['user']['screen_name']
+				users_db[user] = data_user
+				print("\t", user, " updated user database")
 		except:
-			print("Can not save to database")
+			print(data['id_str'], " Can not save to database")
 
 		return True
 
