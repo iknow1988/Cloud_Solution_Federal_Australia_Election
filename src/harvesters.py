a506864c7fbabd3af9e913134f0d9064ffa93800
Kazi Abir Adnan <i_know1988@yahoo.com>
2019-05-01 20:51:27 +1000

    timeline_crawler

--- src/harvesters.py
+++ src/harvesters.py
@@ -1,6 +1,7 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+import tweepy
 import helper
 import json
 from shapely.geometry import shape, Point, Polygon
@@ -58,3 +59,61 @@ class Harvester:
 		stream = Stream(auth, l)
 		stream.filter(track=self.tags, locations=self.boundary, languages=["en"])
 
+
+class TimeLineHarvester(Harvester):
+
+	def __init__(self):
+		auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+		auth.set_access_token(self.access_token, self.access_token_secret)
+		self.api = tweepy.API(auth, wait_on_rate_limit=True, retry_count=3, retry_delay=5, retry_errors=set([401, 404, 500, 503]))
+
+	def get_all_tweets(self,screen_name):
+		all_tweets = []
+		try:
+			new_tweets = self.api.user_timeline(screen_name=screen_name, count=200)
+			all_tweets.extend(new_tweets)
+			oldest = all_tweets[-1].id - 1
+		except:
+			new_tweets = []
+			pass
+		while len(new_tweets) > 0:
+			try:
+				new_tweets = self.api.user_timeline(screen_name=screen_name, count=200, max_id=oldest)
+			except:
+				pass
+			all_tweets.extend(new_tweets)
+			oldest = all_tweets[-1].id - 1
+		count = 0
+
+		for counter, tweet in enumerate(all_tweets):
+			data = tweet._json
+			if helper.tweet_in_australia(data["coordinates"], data['geo'], data['place']):
+				try:
+					self.tweet_db[data['id_str']] = data
+					count = count + 1
+				except:
+					pass
+			# print("Tweet in database")
+			if count and counter % 100 == 0:
+				print("Left to save to database: ", len(all_tweets) - counter)
+		if count:
+			print("Saved %s tweets to database" % count, "for user:", screen_name)
+
+	def start_harvesting(self):
+		rows = len(self.users_db)
+		print(rows)
+		for index, id in enumerate(self.users_db):
+			doc = self.users_db[id]
+			name = doc['screen_name']
+			print(name)
+			if 'processed' not in doc:
+				self.get_all_tweets(name)
+				doc['processed'] = 1
+				try:
+					self.users_db.save(doc)
+					print("Updated %s database" % name)
+				except:
+					print("%s database update error" % name)
+			else:
+				print("User already processed")
+			print("left:", rows - index)
