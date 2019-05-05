3a6373cbeed70ce6fb047d05ac0658522cbb1e14
danielfelipegil <dfgilto@gmail.com>
2019-05-05 13:14:58 +1000

    variable management improvement for ansible tasks in couchdb cluster

--- ansible/run-nectar.sh
+++ ansible/run-nectar.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 source ./openrc/unimelb-comp90024-group-2-openrc.sh
+export PYTHONWARNINGS=ignore::UserWarning
+
 #ansible-playbook -i openstack_prod_inventory.py --private-key ~/.ssh/gild-nectar.pem --ask-become-pass playbook-prod-setup.yml
 ansible-playbook -i openstack_prod_inventory.py --private-key ~/.ssh/gild-nectar.pem -u ubuntu -k -b --become-method=sudo -K playbook-prod-dbcluster.yml 
